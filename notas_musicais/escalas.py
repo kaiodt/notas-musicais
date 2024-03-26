5e37251cc15e6e9c5db5262b46d316a15aa6eb39
Kaio Rocha <kaiodtr@gmail.com>
2024-03-26 20:36:23 -0300

    refactor: mudança do nome da função 'escalas' para 'escala'

--- notas_musicais/escalas.py
+++ notas_musicais/escalas.py
@@ -2,7 +2,7 @@ NOTAS = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 ESCALAS = {'maior': (0, 2, 4, 5, 7, 9, 11)}
 
 
-def escalas(tonica: str, tonalidade: str) -> dict[str, list[str]]:
+def escala(tonica: str, tonalidade: str) -> dict[str, list[str]]:
     """Gera uma escala a partir de uma tônica e uma tonalidade.
 
     Parameters:
@@ -13,10 +13,10 @@ def escalas(tonica: str, tonalidade: str) -> dict[str, list[str]]:
         Dicionário com a lista de notas da escala e os respectivos graus.
 
     Examples:
-        >>> escalas('C', 'maior')
+        >>> escala('C', 'maior')
         {'notas': ['C', 'D', 'E', 'F', 'G', 'A', 'B'], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
 
-        >>> escalas('A', 'maior')
+        >>> escala('A', 'maior')
         {'notas': ['A', 'B', 'C#', 'D', 'E', 'F#', 'G#'], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
 
     """
